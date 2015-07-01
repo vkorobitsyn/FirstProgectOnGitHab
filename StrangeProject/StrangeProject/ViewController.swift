0e6feb2c1997507a6c1061b78eaedd99cca7a2bc
Vitaliy Korobitsyn <vkorobitsyn@macbookpro-23a6.malkosua.com>
2015-07-01 10:27:59 +0300

    add function print

---
+++ StrangeProject/StrangeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StrangeProject
+//
+//  Created by Vitaliy Korobitsyn on 7/1/15.
+//  Copyright (c) 2015 Vitaliy Korobitsyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("start")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
